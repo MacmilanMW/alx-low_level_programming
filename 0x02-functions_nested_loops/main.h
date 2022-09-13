bc07b23ac0abb839de0a45b8af6876514021910a
Macmilan Moya <macmilanmoya@gmail.com>
2022-09-13 05:47:57 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef _main_h_
+#define _main_h_
+
+/*
+ * file -main.h 
+ * Author: M.Moya
+ * Desc: Header contain for prototype for all functions 
+ */
+int _putchar(char c);
+
+
+
+
+#endif
+
