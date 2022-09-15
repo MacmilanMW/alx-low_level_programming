ccf12726362082421b36efb5e4d13746b6194484
Macmilan Moya <macmilanmoya@gmail.com>
2022-09-15 09:33:24 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -13,6 +13,7 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
 
 
 
@@ -23,5 +24,5 @@ void print_most_numbers(void);
 
 
 
-sendif
+#endif
 
