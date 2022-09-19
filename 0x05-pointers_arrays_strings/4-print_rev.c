c918ad60f531eca29184b43fb2f11523fa198960
Macmilan Moya <macmilanmoya@gmail.com>
2022-09-19 09:20:12 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: the used string reference pointer
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int len = strlen(s);
+
+	while (len--)
+		 putchar(*(s + len));
+	putchar(10);
+}
+
